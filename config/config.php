02d744174f75c716d16a7d9cd09505511d67ad0c
divinomeya <divinomeya@gmail.com>
2026-08-26 09:37:31 +0800

    working test-logger

--- config/config.php
+++ config/config.php
@@ -1,37 +1,34 @@
 <?php
-
 session_start();
 
-require_once('../includes/activity-logger.php');
+require_once(__DIR__ . '/../includes/activity-logger.php');
 
 define('BASE_URL', 'http://localhost/it34a');
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'it34a');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-$user_id = "root";
-$user_email = "root";
+$user_id = "root" ?? null;
+$user_email = "root" ?? null;
 
 try {
     $pdo = new PDO(
         "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
         DB_USER,
         DB_PASS,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
 
-    echo "Connection Successful";
-
-    logActivity(
+    $success = logActivity(
         $pdo,
         $user_id,
         $user_email,
-        'connect_db',
+        'db_connect',
         'success'
     );
+
 
 } catch (PDOException $e) {
     die("Connection Failed: " . $e->getMessage());
