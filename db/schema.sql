41bfe5c763939593397b6a71d8492dfd008099c6
divinomeya <divinomeya@gmail.com>
2026-09-09 17:27:57 +0800

    RBAC Setup and Added Insert Query

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,52 @@ CREATE TABLE IF NOT EXISTS activity_logs(
     -- TIMESTAMP
     activity_log_create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- TABLE #3 users table
+CREATE TABLE IF NOT EXISTS users (
+    -- Primary Key for users table
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    -- Initial user details
+    user_email VARCHAR(50) UNIQUE NOT NULL,
+    user_username VARCHAR(20) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_role ENUM('admin', 'manager', 'user') NOT NULL DEFAULT 'user',
+
+    -- User Created Timestamps default not null
+    user_created_at TIMESTAMP 
+        DEFAULT CURRENT_TIMESTAMP,
+
+    -- User updated timestamp
+    user_update_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP 
+);
+
+-- INSERT QUERY
+INSERT INTO users
+(
+    user_email,
+    user_username,
+    user_password,
+    user_role
+)
+VALUES
+(
+    'admin@example.com',
+    'admin',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'admin'
+),
+(
+    'manager@example.com',
+    'manager',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'manager'
+),
+(
+    'user@example.com',
+    'user',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'user'
+);
